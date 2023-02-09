be07e7aee32ea5b86e593e2c99696ac60005ba28
SainiNitin1 <nitinsaini252004@gmail.com>
2023-02-09 21:26:56 +0530

    adding files

---
+++ src/sum2no.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class sum2no {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter 1st no.  ;  ");
+        int num1 = sc.nextInt();
+        System.out.println("enter 2nd no.  :   ");
+        int num2 = sc.nextInt();
+        System.out.println("sum is ;  "+ (num1+num2));
+    }
+}
